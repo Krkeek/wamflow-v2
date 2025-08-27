9073d12ac628450f218abb5f27e2c04295ab4538
Ahmad <ahmadhijazi_me@hotmail.com>
2025-08-27 19:23:32 +0200

    Adjustments

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
-import { HomePage } from './pages/home-page/home-page';
 import { EditorPage } from './pages/editor-page/editor-page';
 import { UnsavedChangesGuard } from './core/guards/UnsavedChangesGaurd';
 
 export const routes: Routes = [
-  { path: '', component: HomePage },
   { path: 'editor', component: EditorPage, canDeactivate: [UnsavedChangesGuard] },
+  { path: '', redirectTo: 'editor', pathMatch: 'full' },
+  { path: '**', redirectTo: 'editor' }
 ];
